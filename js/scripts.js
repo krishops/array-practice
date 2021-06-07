7cb4704fcf1562e4eb65c01b668977244b70e3c6
Kris Hopper <hopperdavis@gmail.com>
2021-06-07 13:44:13 -0700

    finish practicing arrays
    co-authored-by: Dave Lindqvist <lindqvist.dave@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
-
+function newArray(color, movie, song, book) {
+  return [color, movie, song, book];
+}
 
 $(document).ready(function() {
-  $("form#favorites").submit(function(event) {
-    event.preventDefault();
-    
-
-    const color = $("#color").val();
-    const movie = $("#movie").val();
-    const song = $("#song").val();
-    const book = $("#book").val();
-
-    function newArray(color, movie, song, book) {
-      return [color, movie, song, book];
-    }
-
-    let favorites = newArray(color, movie, song, book);
-    
-    console.log(favorites);
-  });
+$("form#favorites").submit(function(event) {
+  event.preventDefault();
   
+
+  const color = $("#color").val();
+  const movie = $("#movie").val();
+  const song = $("#song").val();
+  const book = $("#book").val();
+
+  let favorites = newArray(color, movie, song, book);
+  
+  console.log(favorites);
+
+  // let favoritesTwo = [favorites[1],favorites[0],favorites[2]].toString();
+
+  // console.log(favoritesTwo);
+
+  // let favoritesThree = [];
+  // favoritesThree.push(favoritesTwo);
+  // console.log(favoritesThree);
+
+  $("#output1").text(favorites[0]);
+  $("#output2").text(favorites[1]);
+  $("#output3").text(favorites[2]);
+});
+
 });
